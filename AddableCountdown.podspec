6c183622bd33e15f847101b3809cf8a4bee7dfa5
WAMS M <imimoimoi@gmail.com>
2018-05-17 17:59:13 -0400

    Changed respective access levels to public

--- AddableCountdown.podspec
+++ AddableCountdown.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AddableCountdown'
-  s.version          = '0.1.0'
+  s.version          = '0.1.2'
   s.summary          = 'Class that can trigger a callback after a addable amount of time.'
 
 # This description is used to generate tags and improve search results.
--- AddableCountdown/Classes/ACountdown.swift
+++ AddableCountdown/Classes/ACountdown.swift
@@ -7,13 +7,13 @@
 import Foundation
 
 /// Class that can trigger a callback after a addable amount of time
-final class ACountdown{
+public final class ACountdown{
     
     /// Underlying timer class
     private var timer: Timer = Timer.init()
     
     /// Action to perform whenever the counter hits 0.0
-    var action:(()->())? = nil
+    public var action:(()->())? = nil
     
     /// Get the current vaue of the counter
     private(set) var time: TimeInterval = 0.0{
@@ -27,31 +27,31 @@ final class ACountdown{
     }
     
     /// Countdown's precision in seconds
-    var precision: TimeInterval = 0.5
+    public var precision: TimeInterval = 0.5
     
     /// Initializes a new addable countdown object
     ///
     /// - Parameter action: what to do when the countdown reaches 0
-    init(action:@escaping ()->()){
+    public init(action:@escaping ()->()){
         self.action = action
     }
     
     /// Adds time to the current countdown
     ///
     /// - Parameter time: The ammount of time to add
-    func add(time:TimeInterval){
+    public func add(time:TimeInterval){
         self.time += time
     }
     
     /// Sets a specific value for the current countdown
     ///
     /// - Parameter time: The ammount of time to set
-    func set(time:TimeInterval){
+    public func set(time:TimeInterval){
         self.time = time
     }
     
     /// Fires or resumes the countdown
-    func fire(){
+    public func fire(){
         if #available(iOS 10.0, *) {
             self.timer = Timer.scheduledTimer(withTimeInterval: precision, repeats: true) {[weak self] (_) in
                 self?.time = (self?.time ?? 0) - (self?.precision ?? 1)
@@ -61,13 +61,13 @@ final class ACountdown{
     }
     
     /// Stops the countdown
-    func stop(){
+    public func stop(){
         timer.invalidate()
         self.time = 0
     }
     
     /// Pauses the countdown
-    func pause(){
+    public func pause(){
         timer.invalidate()
     }
 }
